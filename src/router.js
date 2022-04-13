b4fd61ed2c2491b3d641d195f1773bd36db6114e
Safia <safia.ibrahim2121@gmail.com>
2022-04-13 16:47:27 +0100

    Complete

--- src/router.js
+++ src/router.js
@@ -1,48 +1,47 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const { PrismaClient } = require('@prisma/client');
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const router = express.Router();
 
-const jwtSecret = 'mysecretkey';
+const jwtSecret = "mysecretkey";
 
-router.post('/register', async (req, res) => {
-    const { username, password } = req.body;
+router.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  const hashPassword = await bcrypt.hash(password, 10);
+  const createdUser = await prisma.user.create({
+    data: {
+      username: username,
+      password: hashPassword,
+    },
+  });
 
-    const createdUser = await prisma.user.create({
-        data: {
-            username,
-            password
-        }
-    });
-
-    res.json({ data: createdUser });
+  res.json({ data: createdUser.username });
 });
 
-router.post('/login', async (req, res) => {
-    const { username, password } = req.body;
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
 
-    const foundUser = await prisma.user.findFirst({
-        where: {
-            username
-        }
-    });
+  const foundUser = await prisma.user.findFirst({
+    where: {
+      username,
+    },
+  });
 
-    if (!foundUser) {
-        return res.status(401).json({ error: 'Invalid username or password.' });
-    }
+  if (!foundUser) {
+    return res.status(401).json({ error: "Invalid username or password." });
+  }
 
-    const passwordsMatch = password === foundUser.password;
+  const passwordsMatch = await bcrypt.compare(password, foundUser.password);
+  if (!passwordsMatch) {
+    return res.status(401).json({ error: "Invalid username or password." });
+  }
 
-    if (!passwordsMatch) {
-        return res.status(401).json({ error: 'Invalid username or password.' });
-    }
+  const token = jwt.sign({ username }, jwtSecret);
 
-    const token = jwt.sign({ username }, jwtSecret);
-
-    res.json({ data: token });
+  res.json({ data: token });
 });
 
 module.exports = router;
